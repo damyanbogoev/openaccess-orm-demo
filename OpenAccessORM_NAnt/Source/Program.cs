75829b3fccee5c1dd5c8de54ba1206b4fab8315f
Damyan Bogoev <damyan.bogoev@gmail.com>
2013-08-23 00:39:29 +0300

    Adding an integration sample with OpenAccess ORM and NAnt

---
+++ OpenAccessORM_NAnt/Source/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace MyNantTestApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (EntitiesModel context = new EntitiesModel())
+            {
+                Console.WriteLine(context.People.Count());
+            }
+        }
+    }
+}
