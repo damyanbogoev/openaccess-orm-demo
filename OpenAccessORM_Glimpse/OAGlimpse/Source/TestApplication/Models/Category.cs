2907b4b36b4d43dcf15c9a282211ab3a4f24010e
Damyan Bogoev <damyan.bogoev@gmail.com>
2013-10-16 21:38:01 +0300

    Initial commit of the integration sample with Glimpse

---
+++ OpenAccessORM_Glimpse/OAGlimpse/Source/TestApplication/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿namespace TestApplication.Models
+{
+    public class Category
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
